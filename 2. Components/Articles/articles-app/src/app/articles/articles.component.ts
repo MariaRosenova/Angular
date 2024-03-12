dee499f9aedaaa67e1e1bcea19aefa38475318fb
Maria <mimito1353@gmail.com>
2024-03-12 10:27:43 +0200

    add main logic

---
+++ 2. Components/Articles/articles-app/src/app/articles/articles.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../models/article.model';
+import { ArticleData } from '../data/data';
+
+@Component({
+  selector: 'app-articles',
+  standalone: true,
+  imports: [],
+  templateUrl: './articles.component.html',
+  styleUrl: './articles.component.css'
+})
+export class ArticlesComponent implements OnInit {
+  articles: Article[] = [];
+  constructor(){}
+
+  ngOnInit(): void {
+    this.articles = new ArticleData(). getData();
+  }
+}
